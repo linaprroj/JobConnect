d53427ff706cf6be41389d192eb5216f34e4a85e
linaprroj <lpirnraoj@gmail.com>
2024-07-23 17:31:52 -0400

    Add job_seekers model

---
+++ backend/updated_backend/updated_models/job_seekers.py
@@ -0,0 +1,22 @@
+from app_init import db
+
+class JobSeeker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    deleted_at = db.Column(db.DateTime, nullable=True)
+    user = db.relationship('User', back_populates='job_seeker')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'user_id': self.user_id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'deleted_at': self.deleted_at
+        }
